02665b86c3af35a0b5dffe4f4f2661d3a4c3d89c
PutuJUGA <givrantjanuarta@gmail.com>
2026-09-25 22:35:23 +0700

    Typo kurang huruf P di base config.js xixixxi

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-base: '/NitiDong/',
+base: '/NitipDong/',
   plugins: [
     vue(),
     tailwindcss(),
